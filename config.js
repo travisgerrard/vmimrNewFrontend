d61d260cbbc9189ed20e1bfb987ca8d9f5c6559c
Travis Gerrard <travisgerrard@gmail.com>
2019-01-23 20:57:18 -0800

    2nd try cors

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 export const endpoint = `http://localhost:4444`;
-export const prodEndpoint = `https://vmimr-yoga-prod.herokuapp.com`;
+export const prodEndpoint = `https://api.vmimr.com`;
 export const perPage = 12;
