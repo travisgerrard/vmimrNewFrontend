0e03e5d3ba20133d9fabfc18b291f46717bedfe4
Travis Gerrard <travisgerrard@gmail.com>
2019-01-28 15:57:11 -0800

    Redirect on signin

--- components/Signin.js
+++ components/Signin.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import { Mutation } from 'react-apollo';
 import Link from 'next/link';
+import User from './User';
+import Router from 'next/router';
 
 import gql from 'graphql-tag';
 import Form from './styles/Form';
 import Error from './ErrorMessage';
 import { CURRENT_USER_QUERY } from './User';
+import HomeLearning from './HomeLearning';
 
 const SIGNIN_MUTATION = gql`
   mutation SIGNIN_MUTATION($email: String!, $password: String!) {
@@ -30,55 +33,70 @@ class Signin extends Component {
 
   render() {
     return (
-      <Mutation
-        mutation={SIGNIN_MUTATION}
-        variables={this.state}
-        refetchQueries={[{ query: CURRENT_USER_QUERY }]}
-      >
-        {(signup, { error, loading }) => {
+      <User>
+        {({ data: { me } }) => {
+          if (me) {
+            Router.push({
+              pathname: '/'
+            });
+          }
           return (
-            <>
-              <Form
-                method="post"
-                onSubmit={async e => {
-                  e.preventDefault();
-                  const res = await signup();
-                  this.setState({ name: '', email: '', password: '' });
-                }}
-              >
-                <fieldset disabled={loading} aria-busy={loading}>
-                  <h2>Sign into your Account</h2>
-                  <Error error={error} />
-                  <label htmlFor="email">
-                    Virginia Mason Email
-                    <input
-                      type="text"
-                      name="email"
-                      placeholder="email"
-                      value={this.state.email}
-                      onChange={this.saveToState}
-                    />
-                  </label>
-                  <label htmlFor="password">
-                    Password (Cerner Login)
-                    <input
-                      type="password"
-                      name="password"
-                      placeholder="password"
-                      value={this.state.password}
-                      onChange={this.saveToState}
-                    />
-                  </label>
-                  <button type="submit">Sign In!</button>
-                </fieldset>
-              </Form>
-              <Link href="/resetPassword">
-                <a style={{ color: 'blue' }}>Reset Password</a>
-              </Link>
-            </>
+            <Mutation
+              mutation={SIGNIN_MUTATION}
+              variables={this.state}
+              refetchQueries={[{ query: CURRENT_USER_QUERY }]}
+            >
+              {(signup, { error, loading }) => {
+                return (
+                  <>
+                    <Form
+                      method="post"
+                      onSubmit={async e => {
+                        e.preventDefault();
+                        const res = await signup();
+                        this.setState({
+                          name: '',
+                          email: '',
+                          password: ''
+                        });
+                      }}
+                    >
+                      <fieldset disabled={loading} aria-busy={loading}>
+                        <h2>Sign into your Account</h2>
+                        <Error error={error} />
+                        <label htmlFor="email">
+                          Virginia Mason Email
+                          <input
+                            type="text"
+                            name="email"
+                            placeholder="email"
+                            value={this.state.email}
+                            onChange={this.saveToState}
+                          />
+                        </label>
+                        <label htmlFor="password">
+                          Password (Cerner Login)
+                          <input
+                            type="password"
+                            name="password"
+                            placeholder="password"
+                            value={this.state.password}
+                            onChange={this.saveToState}
+                          />
+                        </label>
+                        <button type="submit">Sign In!</button>
+                      </fieldset>
+                    </Form>
+                    <Link href="/resetPassword">
+                      <a style={{ color: 'blue' }}>Reset Password</a>
+                    </Link>
+                  </>
+                );
+              }}
+            </Mutation>
           );
         }}
-      </Mutation>
+      </User>
     );
   }
 }
